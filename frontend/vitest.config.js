e95aebc1ba4bebcec11d26b7a511ac392f038b26
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2026-06-28 13:41:35 -0300

    ..

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom', // Úsalo si pruebas componentes de frontend (React/Vue)
-    globals: true,        // Permite usar 'test' o 'describe' sin importar desde 'vitest'
+    globals: true,  
+    exclude: ['**/node_modules/**', '**/e2e/**'],      // Permite usar 'test' o 'describe' sin importar desde 'vitest'
   },
 })
